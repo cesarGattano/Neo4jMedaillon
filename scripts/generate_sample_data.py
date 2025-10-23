4d5c948b00ade11420efdc789f86ef875ddb23e2
Cesar Gattano <cesar.gattano@gmail.com>
2025-10-23 13:55:13 +0200

    feat: :sparkles: Generate synthetic data

---
+++ scripts/generate_sample_data.py
@@ -0,0 +1,177 @@
+from faker import Faker
+import csv
+from utils import (
+    gen_list_persons,
+    gen_list_organizations,
+    gen_list_papers,
+    compl_nodes,
+    gen_list_works_at,
+    gen_list_has_written,
+    gen_list_has_read,
+)
+
+counter = 0
+
+# Generate the fakers
+fake_en = Faker("en")
+fake_fr = Faker("fr")
+fake_it = Faker("it")
+fake_de = Faker("de")
+fake_es = Faker("es")
+fake_pt = Faker("pt")
+
+# Generate the nodes (~1M)
+# ------------------
+nodes = list()
+
+# 600k persons
+print("generate 600k persons...")
+persons_en, counter = gen_list_persons(150000, fake_en, counter)
+persons_fr, counter = gen_list_persons(110000, fake_fr, counter)
+persons_it, counter = gen_list_persons(100000, fake_it, counter)
+persons_de, counter = gen_list_persons(90000, fake_de, counter)
+persons_es, counter = gen_list_persons(70000, fake_es, counter)
+persons_pt, counter = gen_list_persons(80000, fake_pt, counter)
+
+persons_en_ids = list(persons_en.keys())
+persons_fr_ids = list(persons_fr.keys())
+persons_it_ids = list(persons_it.keys())
+persons_de_ids = list(persons_de.keys())
+persons_es_ids = list(persons_es.keys())
+persons_pt_ids = list(persons_pt.keys())
+
+# max(50k) organisations
+print("generate max(50k) organizations...")
+organizations_en, counter = gen_list_organizations(15000, fake_en, counter)
+organizations_fr, counter = gen_list_organizations(10000, fake_fr, counter)
+organizations_it, counter = gen_list_organizations(8000, fake_it, counter)
+organizations_de, counter = gen_list_organizations(5000, fake_de, counter)
+organizations_es, counter = gen_list_organizations(7000, fake_es, counter)
+organizations_pt, counter = gen_list_organizations(5000, fake_pt, counter)
+
+organizations_en_ids = list(organizations_en.keys())
+organizations_fr_ids = list(organizations_fr.keys())
+organizations_it_ids = list(organizations_it.keys())
+organizations_de_ids = list(organizations_de.keys())
+organizations_es_ids = list(organizations_es.keys())
+organizations_pt_ids = list(organizations_pt.keys())
+
+# Generate 360k papers
+print("generate 360k papers...")
+papers_en, counter = gen_list_papers(180000, fake_en, counter)
+papers_fr, counter = gen_list_papers(40000, fake_fr, counter)
+papers_it, counter = gen_list_papers(40000, fake_it, counter)
+papers_de, counter = gen_list_papers(40000, fake_de, counter)
+papers_es, counter = gen_list_papers(40000, fake_en, counter)
+papers_pt, counter = gen_list_papers(180000, fake_pt, counter)
+
+papers_en_ids = list(papers_en.keys())
+papers_fr_ids = list(papers_fr.keys())
+papers_it_ids = list(papers_it.keys())
+papers_de_ids = list(papers_de.keys())
+papers_es_ids = list(papers_es.keys())
+papers_pt_ids = list(papers_pt.keys())
+
+# Complete nodes
+print("complete nodes...")
+compl_nodes(
+    nodes,
+    [persons_en, persons_fr, persons_it, persons_de, persons_es, persons_pt],
+    "Person",
+)
+compl_nodes(
+    nodes,
+    [
+        organizations_en,
+        organizations_fr,
+        organizations_it,
+        organizations_de,
+        organizations_es,
+        organizations_pt,
+    ],
+    "Org",
+)
+compl_nodes(
+    nodes,
+    [papers_en, papers_fr, papers_it, papers_de, papers_es, papers_pt],
+    "Paper",
+)
+
+# Store in csv
+with open("data/raw/nodes.csv", "w", newline="") as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=",")
+    spamwriter.writerow(["id", "label", "name"])
+    for row in nodes:
+        spamwriter.writerow(row.values())
+
+# Generate edges (~5M)
+# --------------
+edges = list()
+
+# Generate "works at" relation for each persons
+print("generate 'works at' relations...")
+gen_list_works_at(
+    edges,
+    [persons_en, persons_fr, persons_it, persons_de, persons_es, persons_pt],
+    [
+        organizations_en_ids,
+        organizations_fr_ids,
+        organizations_it_ids,
+        organizations_de_ids,
+        organizations_es_ids,
+        organizations_pt_ids,
+    ],
+)
+
+# Generate 1.1M "has written" relations
+print("generate 1.1M 'has written' relations...")
+gen_list_has_written(
+    edges,
+    [300000, 250000, 150000, 200000, 150000, 50000],
+    [
+        persons_en_ids,
+        persons_fr_ids,
+        persons_it_ids,
+        persons_de_ids,
+        persons_es_ids,
+        persons_pt_ids,
+    ],
+    [
+        papers_en_ids,
+        papers_fr_ids,
+        papers_it_ids,
+        papers_de_ids,
+        papers_es_ids,
+        papers_pt_ids,
+    ],
+)
+
+# Generate 2.8M "has red" relations
+print("generate 2.8M 'has read' relations...")
+gen_list_has_read(
+    edges,
+    [500000, 500000, 400000, 500000, 500000, 400000],
+    [
+        persons_en_ids,
+        persons_fr_ids,
+        persons_it_ids,
+        persons_de_ids,
+        persons_es_ids,
+        persons_pt_ids,
+    ],
+    [
+        papers_en_ids,
+        papers_fr_ids,
+        papers_it_ids,
+        papers_de_ids,
+        papers_es_ids,
+        papers_pt_ids,
+    ],
+)
+
+# Store in csv
+with open("data/raw/edges.csv", "w", newline="") as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=",")
+    spamwriter.writerow(["src", "dst", "type"])
+    for row in edges:
+        spamwriter.writerow(row.values())
---
+++ scripts/utils.py
@@ -0,0 +1,312 @@
+from faker import Faker
+import random
+
+
+def gen_list_persons(n_persons: int, my_fake: Faker, counter: int) -> tuple[dict, int]:
+    """Generate a list of persons in the shape of a dictionnary with:
+    {key=id: value=name}.
+
+    Args:
+        n_persons (int): Number of persons
+        my_fake (Faker): Faker generator
+        counter (int): Counter for unique id
+
+    Returns:
+        dict: List of persons
+        int: Counter modified
+    """
+    persons = dict()
+    for _ in range(n_persons):
+        persons[counter] = my_fake.name()
+        counter += 1
+    return persons, counter
+
+
+def gen_list_organizations(
+    n_orgs: int, my_fake: Faker, counter: int
+) -> tuple[dict, int]:
+    """Generate a list of organizations in the shape of a dictionnary with:
+    {key=id: value=name}.
+
+    Args:
+        n_orgs (int): Number of organizations
+        my_fake (Faker): Faker generator
+        counter (int): Counter for unique ids
+
+    Returns:
+        dict: List of organizations
+        int: Counter modified
+    """
+    organizations = dict()
+    for _ in range(n_orgs):
+        company = my_fake.company()
+        if company not in organizations.values():
+            organizations[counter] = company
+            counter += 1
+    return organizations, counter
+
+
+def gen_list_papers(n_papers: int, my_fake: Faker, counter: int) -> tuple[dict, int]:
+    """Generate a list of papers in the shape of a dictionnary with:
+    {key=id, value=name}.
+
+    Args:
+        n_papers (int): Number of papers
+        my_fake (Faker): Faker generator
+        counter (int): Counter for unique ids
+
+    Returns:
+        dict: List of papers
+        int: Counter modified
+    """
+    papers = dict()
+    for _ in range(n_papers):
+        papers[counter] = my_fake.sentence(nb_words=10)
+        counter += 1
+    return papers, counter
+
+
+def compl_nodes(nodes: list[dict], dicts_to_append: list[dict], label: str) -> None:
+    """Append some dictionnaries to the list of nodes **nodes**.
+    Convert in the shape:
+    {id: dict_key, label:**label**, name:dict_value}
+
+    Args:
+        nodes (list[dict]): List of nodes
+        dicts_to_append (list[dict]): List of dictionnaries in the shape {key=id: value=name}
+        label (str): Lable to use
+
+    Returns:
+        None
+    """
+    for my_dict in dicts_to_append:
+        for id, name in my_dict.items():
+            nodes.append({"id": id, "label": label, "name": name})
+    return None
+
+
+def gen_list_works_at(
+    edges: list[dict],
+    lists_persons: list[dict],
+    lists_orgs: list[list[int]],
+) -> None:
+    """Append some edges with 'works at' relations in the shape:
+    {src: id_person, dst: id_org, type: 'works at'}
+
+    Args:
+        edges (list[dict]): List of edges
+        lists_persons (list[dict]): List of persons
+        lists_orgs (list[list[int]]): List of organizations ids
+
+    Returns:
+        None
+    """
+
+    assert len(lists_persons) == len(lists_orgs)
+    for i in range(len(lists_persons)):
+        seq = [j for j in range(len(lists_persons))]
+        seq.pop(i)
+        gen_list_works_at_per_person_list(
+            edges,
+            lists_persons[i],
+            lists_orgs[i],
+            [lists_orgs[j] for j in seq],
+        )
+    return None
+
+
+def gen_list_works_at_per_person_list(
+    edges: list[dict],
+    persons: dict,
+    probable_orgs: list[int],
+    other_orgs: list[list[int]],
+) -> None:
+    """Generate the list of "works at" relations for
+    the list of **persons**. Give higher probability to
+    organizations listed in **probable_orgs** than those
+    listed in **other_orgs**.
+
+    Args:
+        edges (list[dict]): List of edges
+        persons (dict): List of persons
+        probable_orgs (list[int]): List of organizations ids (appearing with higher probability)
+        other_orgs (list[list[int]]): List of organizations ids (appearing with lower probability)
+
+    Returns:
+        None
+    """
+
+    assert len(other_orgs) == 5
+
+    for p in persons.keys():
+        my_dict = dict()
+
+        randn = random.random()
+        if randn <= 0.5:
+            o = random.choice(probable_orgs)
+        elif randn <= 0.6:
+            o = random.choice(other_orgs[0])
+        elif randn <= 0.7:
+            o = random.choice(other_orgs[1])
+        elif randn <= 0.8:
+            o = random.choice(other_orgs[2])
+        elif randn <= 0.9:
+            o = random.choice(other_orgs[3])
+        else:
+            o = random.choice(other_orgs[4])
+
+        my_dict["src"] = p
+        my_dict["dst"] = o
+        my_dict["type"] = "works_at"
+
+        edges.append(my_dict)
+    return None
+
+
+def gen_list_has_written(
+    edges: list[dict],
+    n_new_edges: list[int],
+    lists_persons: list[list[int]],
+    lists_papers: list[list[int]],
+) -> None:
+    """Append some edges with 'has written' relations in the shape:
+    {src: id_person, dst: id_paper, type: 'has written'}
+
+    Args:
+        edges (list[dict]): List of edges
+        n_new_edges list(int): Number of new edges per list of persons
+        lists_persons (list[list[int]]): Lists of person ids
+        lists_papers (list[list[int]]): Lists of paper ids
+
+    Returns:
+        None
+    """
+
+    assert len(lists_persons) == len(lists_papers)
+    assert len(lists_persons) == len(n_new_edges)
+    for i in range(len(lists_persons)):
+        gen_list_has_written_per_person_list(
+            edges,
+            n_new_edges[i],
+            lists_persons[i],
+            lists_papers[0],
+            [lists_papers[i]],
+        )
+    return None
+
+
+def gen_list_has_written_per_person_list(
+    edges: list[dict],
+    n_new_edges: int,
+    persons: list[int],
+    probable_papers: list[int],
+    other_papers: list[list[int]],
+) -> None:
+    """Generate a list of "has writtent" relations between
+    the **persons** listed and the papers listed in **probable_papers**
+    and **other_papers**. A paper in **probable_papers** have better chance to be
+    picked up than the papers in **other_papers**.
+
+    Args:
+        edges (list[dict]): List of edges
+        n_new_edges (int): Number of requested new edges
+        persons (list[int]): List of persons
+        probable_papers (list[int]): List of papers ids (with higher probability to be picked up)
+        other_papers (list[list[int]]): List of papers ids (with lower probability to be picked up)
+
+    Returns:
+        None
+    """
+
+    assert len(other_papers) == 1
+
+    for _ in range(n_new_edges):
+        my_dict = dict()
+
+        p = random.choice(persons)
+        randn = random.random()
+        if randn <= 0.6:
+            o = random.choice(probable_papers)
+        else:
+            o = random.choice(other_papers[0])
+
+        my_dict["src"] = p
+        my_dict["dst"] = o
+        my_dict["type"] = "has_written"
+
+        edges.append(my_dict)
+    return None
+
+
+def gen_list_has_read(
+    edges: list[dict],
+    n_new_edges: list[int],
+    lists_persons: list[list[int]],
+    lists_papers: list[list[int]],
+) -> None:
+    """Append some edges with 'has read' relations in the shape:
+    {src: id_person, dst: id_paper, type: 'has read'}
+
+    Args:
+        edges (list[dict]): List of edges
+        n_new_edges list(int): Number of new edges per list of persons
+        lists_persons (list[list[int]]): Lists of person ids
+        lists_papers (list[list[int]]): Lists of paper ids
+
+    Returns:
+        None
+    """
+
+    assert len(lists_persons) == len(lists_papers)
+    for i in range(len(lists_persons)):
+        gen_list_has_read_per_person_list(
+            edges,
+            n_new_edges[i],
+            lists_persons[i],
+            lists_papers[0],
+            [lists_papers[i]],
+        )
+    return None
+
+
+def gen_list_has_read_per_person_list(
+    edges: list[dict],
+    n_new_edges: int,
+    persons: list[int],
+    probable_papers: list[int],
+    other_papers: list[list[int]],
+) -> None:
+    """Generate a list of "has read" relations between
+    the **persons** listed and the papers listed in **probable_papers**
+    and **other_papers**. A paper in **probable_papers** have better chance to be
+    picked up than the papers in **other_papers**.
+
+    Args:
+        edges (list[dict]): List of edges
+        n_new_edges (int): Number of requested new edges
+        persons (list[int]): List of persons
+        probable_papers (list[int]): List of papers ids (with higher probability to be picked up)
+        other_papers (list[list[int]]): List of papers ids (with lower probability to be picked up)
+
+    Returns:
+        None
+    """
+
+    assert len(other_papers) == 1
+
+    for _ in range(n_new_edges):
+        my_dict = dict()
+
+        p = random.choice(persons)
+        randn = random.random()
+        if randn <= 0.8:
+            o = random.choice(probable_papers)
+        else:
+            o = random.choice(other_papers[0])
+
+        my_dict["src"] = p
+        my_dict["dst"] = o
+        my_dict["type"] = "has_read"
+
+        edges.append(my_dict)
+    return None
